b84996c0d33062ed07c62bb0e82e0815a8a7d7f6
tylersmall <9159598+r3lyts@users.noreply.github.com>
2023-11-03 10:59:55 -0400

    testing

--- src/main/java/Model/Application.java
+++ src/main/java/Model/Application.java
@@ -11,7 +11,7 @@ public class Application extends javafx.application.Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("login-form.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Hello World!");
         stage.setScene(scene);
         stage.show();
     }
